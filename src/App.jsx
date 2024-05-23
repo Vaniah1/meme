b81b3c0e1db29a4e42adc13361227f09546eb545
Vaniah1 <ndunguvaniah@gmail.com>
2024-05-23 07:41:06 +0300

    meme

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import "../src/style.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
